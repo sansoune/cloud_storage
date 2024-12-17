5b0afeaffab0ce7b9ac2ccd2e229c97c0df3cee0
sansoune <yassine.dakhlaoui06@gmail.com>
2024-12-17 03:10:58 +0100

    deleting src folder

--- src/storage/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod disk;
-pub mod cache;
-pub mod compression;
-pub mod retry;
-pub mod validation;
-pub mod progress;
--- src/types/mod.rs
+++
@@ -1,8 +0,0 @@
-mod metadata;
-mod chunk;
-mod file;
-
-pub use chunk::{Chunk, ChunkId};
-pub use file::{FileType, FileTypeDetector, ImageType, DocumentType, VideoType, AudioType};
-pub use metadata::FileMetadata;
-
